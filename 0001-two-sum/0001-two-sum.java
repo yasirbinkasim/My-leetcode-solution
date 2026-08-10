47ba6745150c448b5c1b0a0ce3f3a1bec91eebbe
Mo Yasir <moyasir2005@gmail.com>
2026-08-11 01:25:13 +0530

    Time: 2 ms (99.34%), Space: 47.3 MB (23.71%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(arr[i], i);
+        }
+
+        return new int[]{};
+    }
+}

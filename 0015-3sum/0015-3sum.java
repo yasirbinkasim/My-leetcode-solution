2fbecddfb101de3ae4d7414648d5c04ad84748fe
Mo Yasir <moyasir2005@gmail.com>
2026-09-13 13:42:33 +0530

    Time: 35 ms (41.04%), Space: 59.1 MB (59.75%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,28 +6,27 @@ class Solution {
         Arrays.sort(nums);
         
         for (int i = 0; i < n - 2; i++) {
-            if (nums[i] > 0) break;
             
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             
-            int left = i + 1;
-            int right = n - 1;
+            int j = i + 1;
+            int k = n - 1;
             
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
                 
                 if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
                     
-                    left++;
-                    right--;
+                    j++;
+                    k--;
                 } else if (sum < 0) {
-                    left++; 
+                    j++; 
                 } else {
-                    right--;
+                    k--;
                 }
             }
         }
